eb0a57b3a24bda88d8a6e8f21ac6ec82c22e06d1
Bianca Fu <0509biancafu@gmail.com>
2022-11-24 06:04:28 +0000

    pair programming, searching word horizontally and vertically in 2D array

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
+  if(letters === undefined || word === undefined) return false;
+  if(letters.length === 0) {
+    return false;
+  }
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+    return checkWordHorizontally(horizontalJoin,word) || checkWordHorizontally(verticalJoin, word);
 }
 
+//transpose and check again.
+const transpose = function (matrix) {
+  // Put your solution here 
+  let results = [];
+  const columns = matrix[0].length;
+
+  for (const array of matrix) {
+    for(let j = 0; j<columns;j++) {
+      if (results[j] === undefined) results.push([]);
+      results[j].push(array[j]);
+    }
+  }
+  return results;
+};
+const checkWordHorizontally = (check,word) => {
+  for (l of check) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+//backwards -> reverse string and run again
+
+//diagonally -> 
 module.exports = wordSearch
